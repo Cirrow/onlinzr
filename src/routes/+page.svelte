2bd149d7db6644e18303f49c734ad544e90362d5
cirrow <cirrow@proton.me>
2025-08-21 15:24:40 +1200

    Input component renamed to NameInput

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,7 +1,7 @@
 <script>
-    import Input from "../components/Input.svelte";
+    import NameInput from "../components/NameInput.svelte";
 </script>
 
 <div id="inputDivOuter" class="flex flex-col items-center justify-center min-w-full">
-    <Input />
+    <NameInput />
 </div>
