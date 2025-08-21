11b7b7283f26198737c6ff8a8ad69a16994e7d9f
cirrow <cirrow@proton.me>
2025-08-21 15:22:59 +1200

    tailwind's dark: directive now can be used

--- src/components/Header.svelte
+++ src/components/Header.svelte
@@ -3,12 +3,12 @@
 
     function toggle() {
         dark = !dark
-        window.document.body.classList.toggle('dark-mode')
+        document.documentElement.classList.toggle('dark') // targets <html>    
     }
 </script>
 
 <header class="w-full flex justify-end p-4">
-    <button on:click={toggle} aria-label="Toggle dark mode" class="flex items-center justify-center border rounded-md p-1" class:border-gray-400={dark} class:border-gray-500={!dark}>
+    <button onclick={toggle} aria-label="Toggle dark mode" class="flex items-center justify-center border rounded-md p-1" class:border-gray-400={dark} class:border-gray-500={!dark}>
         {#if dark}
             <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-8">
                 <path stroke-linecap="round" stroke-linejoin="round" d="M21.752 15.002A9.72 9.72 0 0 1 18 15.75c-5.385 0-9.75-4.365-9.75-9.75 0-1.33.266-2.597.748-3.752A9.753 9.753 0 0 0 3 11.25C3 16.635 7.365 21 12.75 21a9.753 9.753 0 0 0 9.002-5.998Z" />
