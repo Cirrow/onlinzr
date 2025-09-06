1322b010f619ef4155f45ae31e0ade5a96d0b37a
cirrow <cirrow@proton.me>
2025-09-06 20:15:06 +1200

    added validateDimensions function
    also added args for capitaliseFirstLetter

--- src/lib/validation.ts
+++ src/lib/validation.ts
@@ -1,10 +1,8 @@
-import { get } from "svelte/store";
-
-import { errorMessage, userFirstName } from "../store";
+import { errorMessage, userFirstName, pack } from "../store";
 import { displayError } from "$lib/helper";
 
-export function capitaliseFirstLetter(): boolean {
-    let str = get(userFirstName)
+export function capitaliseFirstLetter(name: string): boolean {
+    let str = name
     
     const trimmed = str.trim();
 
@@ -29,4 +27,33 @@ export function capitaliseFirstLetter(): boolean {
     // clear old error if successful
     errorMessage.set('');
     return true;
+}
+
+
+export function validateDimensions({ heightInput, widthInput, depthInput }: { heightInput: string; widthInput: string; depthInput: string }): boolean {
+
+    let h = heightInput.trim();
+    let w = widthInput.trim();
+    let d = depthInput.trim();
+
+    if (h === "" || w === "" || d === "") {
+        errorMessage.set(displayError('Dimensions must not be blank.'))
+        return false // blank check
+    
+    }
+    h = h.toLowerCase()
+    w = w.toLowerCase()
+    d = d.toLowerCase()
+
+    if ([h, w, d].some(v => /[a-zA-Z]/.test(v) && !v.endsWith("cm"))) { // contains string but doesnt end in "cm"?
+        errorMessage.set(displayError('Dimensions must not include any other letters other than the units.'))
+        return false
+    }
+
+    if ( [h, w, d].every( v => v.endsWith("cm") && !Number.isNaN(Number(v.slice(0, -2))) ) ) { // ends with cm and is a valid number
+        
+        return true
+    }
+
+    return false
 }
