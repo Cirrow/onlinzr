6c6b0a44911e3097cf09c790bc4390e7826cc4be
cirrow <cirrow@proton.me>
2025-09-06 20:50:03 +1200

    removed unused boilerplate code

--- src/components/NameInput.svelte
+++ src/components/NameInput.svelte
@@ -1,21 +1,14 @@
 <script lang="ts">
-    import { userFirstName } from "../store";
-    import { displayError, handleKeydown } from "$lib/helper";
-    import { capitaliseFirstLetter } from "$lib/validation";
-
-    let name = $state('');
-
-    
+  import { userFirstName } from "../store";
 </script>
 
 <div id="innerinputdiv" class="flex flex-col items-center">
     <span class="pb-10">Please type your first name.</span>
     <input
         id="name"
-        bind:value={name}
+        bind:value={$userFirstName}
         type="text"
         placeholder="Type your name here..."
         class="border border-gray-400 p-2 rounded top-3"
-        onkeydown={(e) => $userFirstName = name}
     />
 </div>
