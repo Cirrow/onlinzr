f031182a2aa3d890e067266f8c7c4970b2e9511a
cirrow <cirrow@proton.me>
2025-09-06 20:49:16 +1200

    added new global state and its corresponding interface

--- src/store.ts
+++ src/store.ts
@@ -5,3 +5,14 @@ import { writable } from 'svelte/store';
 export const userFirstName = writable("") // string type (ts recognises the return of writeable() as Writable<String>)
 
 export const errorMessage = writable("");
+
+
+export interface ReturnPackage {
+    height: string,
+    width: string,
+    depth: string,
+    volume: number
+}
+
+
+export const pack = writable<ReturnPackage>({ height: "", width: "", depth: "", volume: 0}) //the name "package" is taken in strict mode
